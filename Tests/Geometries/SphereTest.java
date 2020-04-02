bfdd1155e280409e51943e1c7b7e3a3b2807b786
Elisheva Aronstam <aronstam@g.jct.ac.il>
2020-04-03 00:20:28 +0300

    test

--- Tests/Geometries/SphereTest.java
+++ Tests/Geometries/SphereTest.java
@@ -1,7 +1,6 @@
 package Geometries;
 
 import Primitives.Point3D;
-import Primitives.Vector;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,14 +9,12 @@ public class SphereTest {
 
 
    /** @Test
-    public void get_radius() {
+    public void get_radius() { ffff
     }*/
 
     @Test
     public void getNormal() {
         Sphere sp=new Sphere(1,new Point3D(0,0,1));
-        assertEquals(new Vector(0.0,0.0,1.0),sp.getNormal(new Point3D(0,0,2)));
-
-
+        assertEquals(new Vector(0,0,1),sp.getNormal(new Point3D(0,0,2)));
     }
 }
