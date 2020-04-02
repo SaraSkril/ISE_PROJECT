94c386deb000386b658180e2dc511599e20f1636
Elisheva Aronstam <aronstam@g.jct.ac.il>
2020-04-02 22:08:23 +0300

    test

--- Tests/Geometries/SphereTest.java
+++ Tests/Geometries/SphereTest.java
@@ -14,6 +14,7 @@ public class SphereTest {
     @Test
     public void getNormal() {
         Sphere sp=new Sphere(1,new Point3D(0,0,1));
+        assertEquals(new Vector(0,0,1),sp.getNormal(new Point3D(0,0,2)));
 
 
     }
