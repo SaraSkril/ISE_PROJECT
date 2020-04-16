53dadbb603237a1e10a0df320851916b4b48dee9
Elisheva Aronstam <aronstam@g.jct.ac.il>
2020-04-16 20:19:49 +0300

    started gemotries class

---
+++ src/Geometries/geomitries.java
@@ -0,0 +1,26 @@
+package Geometries;
+import jdk.jshell.spi.ExecutionControl;
+import Primitives.Point3D;
+import Primitives.Ray;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class geomitries implements Intersectable
+{
+    LinkedList<Intersectable> shapes;
+
+    public geomitries(Intersectable... Geometries)
+    {
+        shapes=new  LinkedList<Intersectable>();
+        /** LinkedList chosen over ArrayList because it is easier to insert and delete items **/
+        if (Geometries.length > 0)
+            add(Geometries);
+    }
+    public void add(Intersectable… Geometries)
+    {
+
+    }
+
+}
