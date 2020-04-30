1d6301932128f89c9312c72d959161779e223f14
Elisheva Aronstam <aronstam@g.jct.ac.il>
2020-04-30 20:44:14 +0300

    teud

--- Tests/elements/integrationTest.java
+++ Tests/elements/integrationTest.java
@@ -11,19 +11,21 @@ public class integrationTest
 {
     Camera cam1 = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0));
     Camera cam2 = new Camera(new Point3D(0, 0, -0.5), new Vector(0, 0, 1), new Vector(0, -1, 0));
-
+    /**
+     * tests sphere and camera ray intersection
+     */
     @Test
     public void constructRayThroughPixelWithSphere1() {
-        //TO DO
-        Sphere sph =  new Sphere(1, new Point3D(0, 0, 3));
+        //sphere has 2 intersection points
+        Sphere sph =  new Sphere(1, new Point3D(0, 0, 3));//builds a sphere which contains a radius and pint3D
 //        Ray ray = cam1.constructRayThroughPixel(3,3,0,0,1,3,3);
 //        List<Point3D> results =  sph.findIntersections(ray);
         List<Point3D> results;
-        int count = 0;
+        int count = 0;//number of intersections ray has with sphere
         int Nx =3;
         int Ny =3;
         for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
+            for (int j = 0; j < 3; ++j) {//loop over places
                 results = sph.findIntsersections(cam1.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3));
                 if (results != null)
                     count += results.size();
@@ -36,7 +38,7 @@ public class integrationTest
     }
     @Test
     public void constructRayThroughPixelWithSphere2()
-    {
+    {//sphere has 18 intersection points
         Sphere sph =  new Sphere(2.5, new Point3D(0, 0, 2.5));
 
         List<Point3D> results;
@@ -47,7 +49,7 @@ public class integrationTest
 
         // TODO explanations
         for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
+            for (int j = 0; j < 3; ++j) {//loop over places
                 results = sph.findIntsersections(cam2.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3));
                 if (results != null)
                     count += results.size();
@@ -59,7 +61,7 @@ public class integrationTest
     }
     @Test
     public void constructRayThroughPixelWithSphere3()
-    {
+    {//sphere has 10 intersection points
         Sphere sph= new Sphere(2,new Point3D(0,0,2));
         List<Point3D> results;
         int count = 0;
@@ -83,7 +85,7 @@ public class integrationTest
 
     @Test
     public void constructRayThroughPixelWithSphere4()
-    {
+    {//sphere has 9 intersection pints
         Sphere sph= new Sphere(4,new Point3D(0,0,1));
         List<Point3D> results;
         int count = 0;
@@ -93,7 +95,7 @@ public class integrationTest
 
         // TODO explanations
         for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
+            for (int j = 0; j < 3; ++j) {//loop over places
                 results = sph.findIntsersections(cam2.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3));
                 if (results != null)
                     count += results.size();
@@ -106,7 +108,7 @@ public class integrationTest
     }
     @Test
     public void constructRayThroughPixelWithSphere5()
-    {
+    {//sphere has 0 intersection points
         Sphere sph= new Sphere(0.5,new Point3D(0,0,-1));
         List<Point3D> results;
         int count = 0;
@@ -116,7 +118,7 @@ public class integrationTest
 
         // TODO explanations
         for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
+            for (int j = 0; j < 3; ++j) {//looop over places
                 results = sph.findIntsersections(cam2.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3));
                 if (results != null)
                     count += results.size();
@@ -152,6 +154,7 @@ public class integrationTest
     @Test
     public void constructRayThroughPixelWithPlane2()
     {
+        // TC02: small angle with view plane (9 points)
         Plane pl= new Plane(new Point3D(3,-3,0), new Point3D(-2,-3,0), new Point3D(0,-1,1.5));
         List<Point3D> results;
         int count = 0;
@@ -174,7 +177,7 @@ public class integrationTest
     }
     @Test
     public void constructRayThroughPixelWithPlane3()
-    {
+    { //TC03: plane has 6 intersection points view plane rays.
         Plane pl= new Plane(new Point3D(0,2,3),new Vector(0,5,5));
         List<Point3D> results;
         int count = 0;
@@ -195,6 +198,9 @@ public class integrationTest
         System.out.println("count: "+count);
 
     }
+    /**
+     * tests triangle and camera ray intersection
+     */
 
     @Test
     public void constructRayThroughPixelWithTriangle1()
@@ -221,7 +227,7 @@ public class integrationTest
     }
     @Test
     public void constructRayThroughPixelWithTriangle2()
-    {
+    {    //TCO2: bigger triangle parallels to plane (2 intersection points)
         Triangle tr=new Triangle(new Point3D(0,-20,2),new Point3D(1,1,2),new Point3D(-1,1,2));
         List<Point3D> results;
         int count = 0;
